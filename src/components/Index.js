fbf6d4b6d1bf41cb717561621adb542a2e9f0237
Lee Warnock <clwarnock@gmail.com>
2016-10-06 07:02:50 -0500

    configures index.js as IndexRoute

---
+++ src/components/Index.js
@@ -0,0 +1,17 @@
+// used by router as IndexRoute
+
+import React, { Component } from 'react';
+
+class IndexComponent extends Component {
+
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <h2>Click on a contact to view their profile</h2>
+    );
+  }
+}
+
+export default IndexComponent;
