dac2be28d4cbfafa0a3ab1d228bd3cd3823fd1c9
Lee Warnock <clwarnock@gmail.com>
2016-10-05 20:05:25 -0500

    sets client ID and cclient domain to app.js new Auth0Lock

--- react-auth/src/components/app.js
+++ react-auth/src/components/app.js
@@ -9,7 +9,7 @@ import { Grid, Row, Col } from 'react-bootstrap';
 class AppComponent extends Component {
 
   componentWillMount() {
-    this.lock = new Auth0Lock('YOUR_AUTH0_CLIENT_ID', 'YOUR_AUTH0_DOMAIN);
+    this.lock = new Auth0Lock('tIzXo5cwrgVER823cSlyfFKHAUSV1S8s', 'leewarnock.auth0.com);
   }
 
   render() {
